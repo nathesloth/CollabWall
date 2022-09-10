da8096ff23a950de19c2c205130b73bc87c538cb
Natalie Smith <nataliesmith4122@gmail.com>
2022-09-10 12:40:10 +1000

    Update and added new sketches

--- collab-wall/public/sketch.js
+++ collab-wall/public/sketch.js
@@ -1,30 +1,73 @@
 let img;
 let img2;
+let img3;
+let img4;
 
 function setup() {
   var canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("containerA");
   img = loadImage("https://i.imgur.com/SMHaUKp.png");
   img2 = loadImage("https://i.imgur.com/deS49jo.png");
+  img3 = loadImage("https://i.imgur.com/5ylLFA4.png");
+  // img4 = loadImage("https://i.imgur.com/2AjlLnZ.png");
+  img4 = loadImage("https://i.imgur.com/m1fTJIG.png");
+
   // frameRate(10); //to make it gradually disappear
 }
 
 function draw() {
   // image(img, 0, 0);
   // background('rgba(250,248,248, 0.4)');// to make it gradually disappear
+
+  // randomStuff();
+}
+
+function randomStuff() {
+  var value = int(random(1, 5));
+  var sizeRandom = int(random(50, 70));
+  if (value == 1) {
+    push();
+    rotate(PI / int(random(-30, 30)));
+    image(img, int(random(0, windowWidth)), int(random(0, windowHeight)), sizeRandom, sizeRandom);
+    pop();
+  }
+  else if (value == 2) {
+    push();
+    rotate(PI / int(random(-30, 30)));
+    image(img2, int(random(0, windowWidth)), int(random(0, windowHeight)), sizeRandom, sizeRandom);
+    pop();
+  }
+  else if (value == 3) {
+    push();
+    rotate(PI / int(random(-30, 30)));
+    image(img3, int(random(0, windowWidth)), int(random(0, windowHeight)), sizeRandom, sizeRandom);
+    pop();
+  }
+  else if (value == 4) {
+    push();
+    rotate(PI / int(random(-30, 30)));
+    image(img4, int(random(0, windowWidth)), int(random(0, windowHeight)), sizeRandom, sizeRandom);
+    pop();
+  }
 }
 
 // mouseMoved
 function mousePressed() {
   // fill(255, 255, 255, 255);
   // ellipse(mouseX, mouseY, 50, 50);
-  var value = int(random(1, 3));
+  var value = int(random(1, 4));
   var sizeRandom = int(random(50, 70));
   if (value == 1) {
     image(img, mouseX - 25, mouseY - 25, sizeRandom, sizeRandom);
   }
   else if (value == 2) {
     image(img2, mouseX - 25, mouseY - 25, sizeRandom, sizeRandom);
+  }
+  else if (value == 3) {
+    image(img3, mouseX - 25, mouseY - 25, sizeRandom, sizeRandom);
+  }
+  else if (value == 4) {
+    image(img4, mouseX - 25, mouseY - 25, sizeRandom, sizeRandom);
   }
   console.log("Clicked");
 }
